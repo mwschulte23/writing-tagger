1e1c039199dcdfb88457bccf24694240021c9834
Mike Schulte <mikeschulte@mikes-mac-mini.lan>
2024-11-17 20:58:21 -0600

    user add tags

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
-from pydantic import BaseModel, Field
+from enum import Enum
+from pydantic import BaseModel, Field, create_model
 from typing import List, Optional, Literal
 from dotenv import load_dotenv
 load_dotenv()
@@ -7,19 +8,29 @@ load_dotenv()
 from helpers.helpers import load_config, hydrate_prompt, make_llm_call, setup_user_detail
 
 
-class Tag(BaseModel):
-    sketchpad: str = Field(description='140 character or less thought process for choosing tag')
-    tag: Literal["emotional", "physical", "social", "spiritual", "professional", "personal", "financial"]
-
-class Tags(BaseModel):
-    tags: List[Tag] = Field(description='List of up to 4 tags that best represent the user\'s input.')
-
-
 def write_html_tags(tags: List[str]):
     html_tags = ''
     for tag in tags:
         html_tags += f'<span style="background-color: #3b82f6; color: white; border-radius: 4px; margin: 0 2px; padding: 0 8px;">{tag}</span>'
     return html_tags
+
+
+def create_enum(values: list):
+    return Enum('TagEnum', {v.upper(): v for v in values})
+
+def choose_tags() -> Enum:
+    with open('tags.txt', 'r') as f:
+        TAGS = [i.strip() for i in f.readlines()]
+    tags = st.sidebar.text_input('Tag', placeholder='Enter tags, separated by commas')
+    add_tag = st.sidebar.button('Add Tag')
+
+    if add_tag:
+        TAGS.extend([i.strip() for i in tags.split(',')])
+    st.sidebar.write(TAGS)
+    TAGS = list(set(TAGS))
+    with open('tags.txt', 'w') as f:
+        f.write('\n'.join(TAGS))
+    return create_enum(TAGS)
 
 
 def main():
@@ -34,6 +45,20 @@ def main():
     user_input = st.text_area('User Input', height=200)
 
     submit = st.button('Submit')
+    tag_enum = choose_tags()
+    # print([i for i in tag_enum])
+    # class TagEnum(Enum):
+    #     __members__ = tag_enum.__members__
+    
+    Tag = create_model(
+        'Tag',
+        tag=(tag_enum, ...)  # (type, default_value)
+    )
+    
+    Tags = create_model(
+        'Tags',
+        tags=(List[Tag], ...)  # (type, default_value)
+    )
 
     if submit and user_input:
         response = make_llm_call(
@@ -41,9 +66,10 @@ def main():
             system_prompt=system_prompt,
             response_model=Tags
         )
-        response_tags = [tag.tag for tag in response.tags]
+        print(response)
+        response_tags = [tag.tag.value for tag in response.tags]
         st.markdown(write_html_tags(response_tags), unsafe_allow_html=True)
+
+
 if __name__ == "__main__":
     main()
-
-
